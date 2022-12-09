cfa0c2058084f4b33da6adc7e4edd4796543abae
Helmut Roderus <helmut.roderus@hs-ansbach.de>
2022-12-09 20:08:02 +0100

    FileStorageDemo - aktualisiert im Dezember 2022

--- app/src/main/java/de/planetmuk/android/filestoragedemo/MainActivity.java
+++ app/src/main/java/de/planetmuk/android/filestoragedemo/MainActivity.java
@@ -2,12 +2,9 @@ package de.planetmuk.android.filestoragedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
